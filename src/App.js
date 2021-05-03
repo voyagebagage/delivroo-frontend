5211235e13c9174345b11c1aa537c0588c6ca04b
Olivier Frugier <sedatif2@MacBook-Pro-de-Olivier.local>
2021-05-03 19:04:14 +0700

    w/ start code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React, { useState, useEffect } from "react";
+
 import "./App.css";
 
+import axios from "axios";
+
 function App() {
-  return (
-    <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
-    </div>
+  const [data, setData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:3100/");
+    // console.log(response.data);
+    setData(response.data);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return isLoading ? (
+    <span>En cours de chargement... </span>
+  ) : (
+    <h2>{data.restaurant.name}</h2>
   );
 }
 
